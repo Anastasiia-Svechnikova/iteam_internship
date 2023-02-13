246cd215de99abbea50c4fa0649af48749bb4911
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2023-02-13 17:19:57 +0200

    fixed wait-resume logic using BehaviorSubject

--- src/app/stopwatch/stopwatch.component.ts
+++ src/app/stopwatch/stopwatch.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { interval, map, Observable, Subscription } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, interval, map, Observable, startWith, Subscription, tap } from 'rxjs';
 import { transformTime } from '../helpers/timeTransformHelper';
 
 @Component({
@@ -7,44 +7,71 @@ import { transformTime } from '../helpers/timeTransformHelper';
   templateUrl: './stopwatch.component.html',
   styleUrls: ['./stopwatch.component.scss']
 })
-export class StopwatchComponent  implements OnInit {
+export class StopwatchComponent  implements OnInit, OnDestroy {
   hours: number = 0
   minutes: number = 0
   seconds = 0
   activeMode = false;
-  subscription!: Subscription
-  obs$= interval(1000)
+  waitingMode = false;
+  subscriptionStart!: Subscription
+  subscriptionResume!: Subscription
+  stopWatch$ = new BehaviorSubject(0)
+  stopWatchSubscription = new Subscription()
+  interval$ = interval(1000)
+  previous = 0;
+  
   
   
   ngOnInit(): void {
+    this.stopWatchSubscription = this.stopWatch$.subscribe((val ) => {
+      this.hours = transformTime(val).hours
+      this.minutes = transformTime(val).minutes
+      this.seconds = transformTime(val).seconds
+    })
    
   }
 
   onStart() {
-    // const initialTime = Date.now()
     if (!this.activeMode) {
-      this.subscription = this.obs$
-      //   .pipe(map((el) => el)
-      // )
-      .subscribe(total => {
-        this.hours = transformTime(total).hours
-        this.minutes = transformTime(total).minutes
-        this.seconds = transformTime(total).seconds
-      })
+      this.subscriptionStart = this.interval$.subscribe(this.stopWatch$)
       this.activeMode = true;
 
     } else {
-      this.subscription.unsubscribe()     
+      
+      this.activeMode = false;
+      this.waitingMode = false;
+      this.subscriptionStart.unsubscribe()   
     }
   }
 
 
   onReset() {
+    this.previous = 0
     this.activeMode = false;
-    this.subscription.unsubscribe()
+    this.waitingMode = false;
+    this.subscriptionStart.unsubscribe()
     this.hours = 0;
     this.minutes = 0;
     this.seconds = 0;
+  }
+
+  onWait() {
+    if (!this.waitingMode) {
+      
+      this.previous = this.stopWatch$.value;
+      console.log(this.previous)
+      this.subscriptionStart.unsubscribe()
+      this.waitingMode = true;
+    } else {
+       this.waitingMode = false;
+      this.subscriptionStart = this.interval$.pipe(map((val)=>val + this.previous)).subscribe(this.stopWatch$)
+      console.log(this.subscriptionStart)
+    }
+  }
+
+
+  ngOnDestroy(): void {
+    this.stopWatchSubscription.unsubscribe()
   }
 
 }
